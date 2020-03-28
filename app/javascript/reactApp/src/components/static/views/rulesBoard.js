5a6dfbd86491dc3656eae070c70de870034d50af
Binod Tamang <bakamaru_binod@hotmail.com>
2020-03-29 00:10:42 +0545

    heroku hosting production fixes

--- app/javascript/reactApp/src/components/static/views/rulesBoard.js
+++ app/javascript/reactApp/src/components/static/views/rulesBoard.js
@@ -5,14 +5,13 @@ export default class RulesBoard extends React.Component {
         return (<section id="instructions-cont">
             <div id="instructions">
                 <h3 className="instr-text">RULES SCORING</h3>
-                <p className="instr-text">A player has three minutes to find as many words as they can in the grid, according to the following rules:</p>
+                <p className="instr-text">A player has 3 minutes to find as many words as they can in the grid, according to the following rules:</p>
                 <ul className="instr-list">
                     <li>The letters must be adjoining in a 'chain'.</li>
                     <li>Words must contain at least three letters.</li>
                     <li>No letter cube may be used more than once within a single word.</li>
                     <li>Click letters in order then 'Add Word'</li>
-                    <li>Your entry will be checked using an online API</li>
-                    <li>The Timer will start after clicking the 'Shake' button</li>
+                    <li>Your entry will be checked using an online API</li>                   
                 </ul>
 
                 <p className="instr-text">Scoring:</p>
