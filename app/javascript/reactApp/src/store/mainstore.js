3eca9809b725a3f5a1cff4c65c40c0ca0896434d
Binod Tamang <bakamaru_binod@hotmail.com>
2020-03-28 23:05:09 +0545

    quick fix

--- app/javascript/reactApp/src/store/mainstore.js
+++ app/javascript/reactApp/src/store/mainstore.js
@@ -1,10 +1,10 @@
 import { createBrowserHistory } from 'history';
 import configureStore from './configureStore';
-import { ApplicationState } from './index';
+import { reducers } from './index';
 // Create browser history to use in the Redux store
 //const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
 const history = createBrowserHistory({ basename: "/" });
 // Get the application-wide store instance, prepopulating with state from the server where available.
-const initialState = ApplicationState;//(window).initialReduxState;
+const initialState = reducers;//(window).initialReduxState;
 const store = configureStore(history, initialState);
 export default store;
