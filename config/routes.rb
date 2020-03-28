d25402bb836a405b2504d3b399a5da7a672fd5b3
Binod Tamang <bakamaru_binod@hotmail.com>
2020-03-28 21:55:19 +0545

    updated configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  match '/token/get',    to: 'token#issue', via: :post
+  get ':controller(/:action(/:id))'
+ 
+  
+# namespace :api do
+#     match ":controller(/:action(/*params))", via: [:get, :post]
+# end
+  get '*path', to: 'home#index' #redirect to react rout
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
